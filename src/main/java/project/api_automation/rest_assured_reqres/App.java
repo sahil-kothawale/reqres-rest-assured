c673532fc6902f614a2bfea1428dfa7a0258f03e
Sahil <sahilkothawale26@gmail.com>
2025-06-14 21:55:37 +0530

    initial commit

---
+++ src/main/java/project/api_automation/rest_assured_reqres/App.java
@@ -0,0 +1,13 @@
+package project.api_automation.rest_assured_reqres;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
